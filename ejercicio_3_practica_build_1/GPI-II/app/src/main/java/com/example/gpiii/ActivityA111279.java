05b545f223ac83aeb31f1d4f709f6d8b1a85a0e1
Mikel Romano <romano.111279@e.unavarra.es>
2021-04-16 07:04:45 +0000

    Añadidos ficheros practica 3

---
+++ ejercicio_3_practica_build_1/GPI-II/app/src/main/java/com/example/gpiii/ActivityA111279.java
@@ -0,0 +1,14 @@
+package com.example.gpiii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityA111279 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
